4d2a2f558de58cda7711b758d9232b916f701dbc
Bashir Ahmed <bashir0609@users.noreply.github.com>
2025-07-03 21:03:38 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,5 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <style jsx global>{`
-        body {
-          background: linear-gradient(135deg, #3b82f6, #4f46e5);
-          font-family: system-ui, sans-serif;
-          color: #1e293b;
-          margin: 0;
-          padding: 0;
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
